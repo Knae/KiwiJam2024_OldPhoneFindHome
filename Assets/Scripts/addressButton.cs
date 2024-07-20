6c8f97c1e29e5d5e40550edd59af9479fe1db841
Sezart987 <allen.vong2020@gmail.com>
2024-07-20 13:53:21 +1200

    add uber funct

---
+++ Assets/Scripts/addressButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class addressButton : MonoBehaviour
+{
+    public string input;
+    public addressPopUp addressPopUp;
+
+    public void setPopUpText()
+    { 
+        addressPopUp.setText(input);
+    }
+}
---
+++ Assets/Scripts/addressPopUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class addressPopUp : MonoBehaviour
+{
+    public TMP_Text text;
+
+    public void setText(string input) 
+    {
+        text.text = input;
+    }
+
+}
