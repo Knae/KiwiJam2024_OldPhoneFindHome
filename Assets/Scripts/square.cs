c92d03fcfad235f6050e5662b369742723c031ab
PK-5 <karumalil.p@maristcollege.school.nz>
2024-07-20 12:18:45 +1200

    UI Commit - Abbey + Princya

---
+++ Assets/Scripts/square.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class square : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
