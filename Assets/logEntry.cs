6889e42f3dd2d459d7f9cbc20e0ebd823a48073d
Sezart987 <allen.vong2020@gmail.com>
2024-07-21 14:10:36 +1200

    Add descriptions to phone logs

--- Assets/logEntry.cs
+++ Assets/logEntry.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class logEntry : MonoBehaviour
 {
+    public PopupHint hint;
     public bool randomizedName = true;
     public bool randomizedIcon = true;
-    public string[] logNames = { "Mark", "bob" };
+    public string[] logNames = { "Mark", "bob", "Asshat" };
+    public string[] descriptions = { "Mark's Description", "bob's description", "I don't like this guy"};
     public TMP_Text text;
     public GameObject callIcon;
 
@@ -16,7 +18,7 @@ public class logEntry : MonoBehaviour
         {
             int randomNumber = Random.Range(0, logNames.Length);
             text.text = logNames[randomNumber];
-
+            hint._hintText = descriptions[randomNumber];
         }
 
         if (randomizedIcon) 
